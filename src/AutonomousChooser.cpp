d5433f374c48526f1b23c28c081e7fdd4bebc8cb
Mat <matfights@gmail.com>
2018-01-29 19:56:37 -0600

    Added SuspendableThread class

--- src/AutonomousChooser.cpp
+++ src/AutonomousChooser.cpp
@@ -7,66 +7,38 @@
 
 #include <AutonomousChooser.h>
 #include "RevRoboticsDigitBoard.h"
+#include "SuspendableThread.h"
 #include <string>
 
 AutonomousChooser::AutonomousChooser()
-    : mCounter( 0u )
-    , mRun( true )
-    // Creates a thread that will run the function ThreadBody(). Since that
-    // function is a member of ExampleDeviceThread we need to pass the
-    // this pointer. All non-static member functions have an implicit first
-    // argument of the class type.
-    , mThread( &AutonomousChooser::ThreadBody, this )
-	, currentSelection( 1 )
+	: currentSelection( 1 )
 {
+	SetRunDelay(std::chrono::milliseconds(100));
+	Run();
 }
 
 AutonomousChooser::~AutonomousChooser() {
-	// Indicate we want the thread to stop.
-    StopThread();
-    // Wait for the thread to stop. The function joinable() is used to
-    // ensure mThread is not the current thread and contains a valid
-    // thread. There are cases where std::thread does not represent a
-    // valid execution context.
-    if( mThread.joinable() )
-    {
-        mThread.join();
-    }
+
 }
 
-uint32_t AutonomousChooser::GetCount() const
-{
-	return mCounter;
-}
 
-void AutonomousChooser::StopThread()
+void AutonomousChooser::OnRun()
 {
-    mRun = false;
-}
 
-void AutonomousChooser::ThreadBody()
-{
 	REVDigitBoard RRDB;
-	bool OldA = false;
+	static bool OldA = false;
+	if( RRDB.getButtonA() == true && OldA == false){
+		currentSelection++;
+	}
+	if( currentSelection == 4 ){
+		currentSelection = 1;
+	}
+	// This could be anything. But for example purposes I am just
+	// incrementing a counter value to demonstrate the thread is
+	// running even though main() never increments the counter.
+	OldA = RRDB.getButtonA();
+	RRDB.display(std::to_string(currentSelection));
 
-
-
-	while( true == mRun )
-    {
-		if( RRDB.getButtonA() == true && OldA == false){
-			currentSelection++;
-		}
-		if( currentSelection == 4 ){
-			currentSelection = 1;
-		}
-        // This could be anything. But for example purposes I am just
-        // incrementing a counter value to demonstrate the thread is
-        // running even though main() never increments the counter.
-        mCounter += 1;
-        OldA= RRDB.getButtonA();
-        RRDB.display(std::to_string(currentSelection));
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
 
 }
 
--- src/AutonomousChooser.h
+++ src/AutonomousChooser.h
@@ -8,38 +8,22 @@
 #ifndef SRC_AUTONOMOUSCHOOSER_H_
 #define SRC_AUTONOMOUSCHOOSER_H_
 
+#include "SuspendableThread.h"
 #include <atomic>   // For atomic variables.
 #include <chrono>   // For millisecond duration type.
 #include <cstdint>  // For standard type definitions.
 #include <iomanip>  // For hex, setw, and setfill.
 #include <thread>   // For thread and sleep_for.
 
-class AutonomousChooser {
+class AutonomousChooser: public SuspendableThread {
 public:
     AutonomousChooser();
     virtual ~AutonomousChooser();
-    uint32_t GetCount() const;
-    void StopThread();
     int AutonomousSelection();
 private:
-    void ThreadBody();
-    // The order I declared these in is important because it is the order the
-    // copmiler will initialize them in. We do not want the thread body to
-    // start before the variables are initialized.
-
-    //! Atomic variables need to be of limited fixed sizes. They are modified
-    //! with special functions that use CPU primitives to update without the
-    //! need of operating system locks.
-    std::atomic_uint mCounter;
-
-    //! Thread loop control variable. The thread will run while this is true
-    //! and exit when it becomes false.
-    std::atomic_bool mRun;
-
-    //! Object encapsulating an execution thread.
-    std::thread mThread;
-
     int currentSelection;
+protected:
+    void OnRun();
 };
 
 #endif /* SRC_AUTONOMOUSCHOOSER_H_ */
